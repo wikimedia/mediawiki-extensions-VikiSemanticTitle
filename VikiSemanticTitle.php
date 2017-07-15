e953a93a516ca2e1990b829e0b74ba43b5c9b60d
Umherirrender <umherirrender_de.wp@web.de>
2017-07-15 23:09:24 +0200

    Remove i18n shim
    It is not used by the new extension registration
    
    Change-Id: I1456f052b3266b37e1c038ae36010b5f8904dbc3

--- VikiSemanticTitle.php
+++ VikiSemanticTitle.php
@@ -31,7 +31,6 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'VikiSemanticTitle' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['VikiSemanticTitle'] = __DIR__ . "/i18n";
-	$wgExtensionMessagesFiles['VikiSemanticTitle'] = __DIR__ . '/VikiSemanticTitle.i18n.php';
 	wfWarn(
 		'Deprecated PHP entry point used for VikiSemanticTitle extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
